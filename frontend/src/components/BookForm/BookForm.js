760355a45f353321f1e1434f887938fb1e05443e
Stalmax76 <68605477+Stalmax76@users.noreply.github.com>
2023-11-05 21:09:25 +0100

    created booksSlice

--- frontend/src/components/BookForm/BookForm.js
+++ frontend/src/components/BookForm/BookForm.js
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import { useDispatch } from 'react-redux';
-
-import { addBook } from '../../redux/books/actionCreators';
+import { addBook } from '../../redux/slices/booksSlice';
+// import { addBook } from '../../redux/books/actionCreators';
 import booksData from '../../data/books.json';
 import './BookForm.css';
 import createBookWithId from '../../utils/createBookWithId';
