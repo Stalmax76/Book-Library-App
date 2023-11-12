d0d6120a6f10013c8ff6d3881c90a024e67955a7
Stalmax76 <68605477+Stalmax76@users.noreply.github.com>
2023-11-12 20:31:30 +0100

    Added isLoading and spinner in the BookForm

--- api/index.js
+++ api/index.js
@@ -5,12 +5,22 @@ const booksData = require('./data/books.json');
 const app = express();
 
 app.use(cors());
-
-app.get('/random-book', (reg, res) => {
+function getRandomBook() {
   const randomIndex = Math.floor(Math.random() * booksData.length);
   const randomBook = booksData[randomIndex];
-  res.json(randomBook);
+  return randomBook;
+}
+
+app.get('/random-book', (reg, res) => {
+  res.json(getRandomBook());
 });
+
+app.get('/random-book-delayed', (reg, res) => {
+  setTimeout(() => {
+    res.json(getRandomBook());
+  }, 3000);
+});
+
 const port = process.env.PORT || 4000;
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
